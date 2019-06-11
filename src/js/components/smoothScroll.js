495397b4f4561f59f99f97fa1a753beb5f231ca6
Viktor Daňko <viktor.danko@superkoders.com>
2019-06-11 07:46:16 +0200

    Close a mobile menu after click on anchor

--- src/js/components/smoothScroll.js
+++ src/js/components/smoothScroll.js
@@ -1,5 +1,9 @@
 export const init = () => {
 
+	const header = document.querySelector('.header');
+	const menu = document.querySelector('.m-main');
+	const btn = document.querySelector('.js-toggleMobileMenu');
+
 	document.querySelectorAll('.js-smooth-scroll').forEach(link =>  {
 		link.addEventListener('click', function(event) {
 			event.preventDefault();
@@ -7,6 +11,11 @@ export const init = () => {
 			document.querySelector(this.getAttribute('href')).scrollIntoView({
 				behavior: 'smooth',
 			});
+
+			header.classList.remove('is-opened');
+			menu.classList.remove('is-opened');
+			btn.classList.remove('is-opened');
+
 		});
 	});
 
