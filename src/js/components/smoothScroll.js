dd2ab01075485b30d44000e4ddf999fc0a8f351d
Viktor Daňko <viktor.danko@superkoders.com>
2019-06-13 15:34:48 +0200

    Smooth scrolling as default

--- src/js/components/smoothScroll.js
+++ src/js/components/smoothScroll.js
@@ -4,18 +4,11 @@ export const init = () => {
 	const menu = document.querySelector('.m-main');
 	const btn = document.querySelector('.js-toggleMobileMenu');
 
-	document.querySelectorAll('.js-smooth-scroll').forEach(link =>  {
-		link.addEventListener('click', function(event) {
-			event.preventDefault();
-
-			document.querySelector(this.getAttribute('href')).scrollIntoView({
-				behavior: 'smooth',
-			});
-
+	document.querySelectorAll('.js-smooth-scroll').forEach(link => {
+		link.addEventListener('click', function() {
 			header.classList.remove('is-opened');
 			menu.classList.remove('is-opened');
-			btn.classList.remove('is-opened');
-
+			btn.setAttribute('aria-expanded', false);
 		});
 	});
 
