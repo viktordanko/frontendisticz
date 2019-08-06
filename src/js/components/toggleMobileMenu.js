5092a19c37e75ff9c7727b3d8846daac7087ee07
Viktor Daňko <viktor.danko@superkoders.com>
2019-08-06 16:20:31 +0200

    Remove not necessary add/remove class when togglin mobile menu

--- src/js/components/toggleMobileMenu.js
+++ src/js/components/toggleMobileMenu.js
@@ -1,17 +1,14 @@
 export const init = () => {
 
 	const btn = document.querySelector('.js-toggleMobileMenu');
-	const menu = document.querySelector('.m-main');
 	const header = document.querySelector('.header');
 
 	const toggleMenu = (open) => {
 		btn.setAttribute('aria-expanded', open);
 		if (open) {
-			menu.classList.add('is-opened');
 			header.classList.add('is-opened');
 			document.addEventListener('click', handleClickOutside);
 		} else  {
-			menu.classList.remove('is-opened');
 			header.classList.remove('is-opened');
 			document.removeEventListener('click', handleClickOutside);
 		}
