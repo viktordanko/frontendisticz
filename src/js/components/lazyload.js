afdf419a042dfc165aa3b4263517203e1b76a98d
Viktor Daňko <viktor.danko@superkoders.com>
2019-12-10 15:53:12 +0100

    Init downloading an img sooner with lazyload

--- src/js/components/lazyload.js
+++ src/js/components/lazyload.js
@@ -1,11 +1,14 @@
 export const init = () => {
 
 	const items = document.querySelectorAll('[data-src], [data-srcset]');
-
 	// Check if IntersectionObserver is supported
 	if ('IntersectionObserver' in window &&
 	'IntersectionObserverEntry' in window &&
 	'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
+		const options = {
+			rootMargin: '100px',
+			threshold: 0.25,
+		};
 		const observer = new IntersectionObserver((entries) => {
 			entries.forEach((entry) => {
 				if (entry.isIntersecting === false) return;
@@ -13,7 +16,7 @@ export const init = () => {
 				entry.target.src = entry.target.dataset.src;
 				observer.unobserve(entry.target);
 			});
-		});
+		}, options);
 
 		[...items].forEach((img) => {
 			observer.observe(img);
