fcc5ba0352b879e16597b5be0084bc153cb79784
Viktor Daňko <viktor.danko@superkoders.com>
2021-04-07 12:30:03 +0200

    Close mobile menu when it's clicked on link

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -2,6 +2,7 @@ export const init = () => {
 	const { body } = document;
 	const button = document.querySelector('[data-menu-btn]');
 	const navigation = document.querySelector('nav');
+	const menu = document.querySelector('.menu');
 
 	if (!button) return;
 
@@ -14,6 +15,11 @@ export const init = () => {
 		navigation.setAttribute('aria-expanded', isOpened);
 		if (isOpened) {
 			body.dataset.state = 'menu-opened';
+			menu.addEventListener('click', (event) => {
+				if (event.target.className !== 'm-main__link') return;
+				body.removeAttribute('data-state');
+				navigation.removeAttribute('aria-expanded');
+			});
 		} else {
 			body.dataset.state = '';
 		}
