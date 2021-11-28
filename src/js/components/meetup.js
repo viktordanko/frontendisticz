ef00510207a53d64a60fdb3dc3fc1b1cb6d2c2e1
Viktor Danko <vik@panter.ch>
2021-11-28 15:07:00 +0100

    Display next meetup event

--- src/js/components/meetup.js
+++ src/js/components/meetup.js
@@ -4,7 +4,7 @@ export const init = async () => {
 	if (!el) return;
 
 	try {
-		const response = await fetch('https://api.meetup.com/frontendisti/', {
+		const response = await fetch('https://api.meetup.com/frontendisti/events', {
 			headers: {
 				'Access-Control-Allow-Origin': '*',
 			},
@@ -17,13 +17,26 @@ export const init = async () => {
 
 		const data = await response.json();
 
-		console.log(data);
+		const meetupName = data[0].name;
+		const meetupDate = new Date(data[0].local_date).toLocaleDateString('cs-CZ', {}).replace(/\//g, '.');
+		const meetupTime = data[0].local_time;
+		const meetupLink = data[0].link;
 
-		const name = document.createElement('p');
-		name.innerText = data[0].name;
-		el.appendChild(name);
+		document.querySelector('[data-meetup-name]').classList.remove('inline-skeleton');
+		document.querySelector('[data-meetup-date]').classList.remove('inline-skeleton');
+		document.querySelector('[data-meetup-name]').innerText = meetupName;
+		document.querySelector('[data-meetup-date]').innerText = `${meetupDate} – ${meetupTime}`;
+		document.querySelector('[data-meetup-link]').href = meetupLink;
+
+		setTimeout(() => {
+			document.querySelector('[data-meetup-name]').classList.remove('inline-skeleton');
+			document.querySelector('[data-meetup-date]').classList.remove('inline-skeleton');
+		}, 10000);
 
 	} catch (error) {
+		document.querySelector('[data-meetup-name]').classList.remove('inline-skeleton');
+		document.querySelector('[data-meetup-date]').classList.remove('inline-skeleton');
+		document.querySelector('[data-meetup-name]').innerText = 'Nepodařilo se načíst data';
 		console.error(error);
 	}
 };
