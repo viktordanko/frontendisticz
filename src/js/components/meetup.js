18cc9fa3d0eab046a8a01bb9e61a479cb00ff76c
Viktor Danko <vik@panter.ch>
2021-11-28 16:55:18 +0100

    Temporary replace meetup data with static one

--- src/js/components/meetup.js
+++ src/js/components/meetup.js
@@ -6,7 +6,7 @@ export const init = async () => {
 	try {
 		const response = await fetch('https://api.meetup.com/frontendisti/events', {
 			headers: {
-				'Access-Control-Allow-Origin': 'https://frontendisti.cz/',
+				'Access-Control-Allow-Origin': '*',
 			},
 		});
 
@@ -28,16 +28,12 @@ export const init = async () => {
 		document.querySelector('[data-meetup-date]').innerText = `${meetupDate} – ${meetupTime}`;
 		document.querySelector('[data-meetup-link]').href = meetupLink;
 
-		setTimeout(() => {
-			document.querySelector('[data-meetup-name]').classList.remove('inline-skeleton');
-			document.querySelector('[data-meetup-date]').classList.remove('inline-skeleton');
-		}, 10000);
-
 	} catch (error) {
+		console.error(error);
+
 		document.querySelector('[data-meetup-name]').classList.remove('inline-skeleton');
 		document.querySelector('[data-meetup-date]').classList.remove('inline-skeleton');
 		document.querySelector('[data-meetup-name]').innerText = 'Nepodařilo se načíst data';
-		console.error(error);
 	}
 };
 
