caef966f0299955302ffdaa8f3056c22310724ba
Viktor Danko <vik@panter.ch>
2021-11-30 11:49:37 +0100

    rm headers

--- src/js/components/meetup.js
+++ src/js/components/meetup.js
@@ -4,12 +4,7 @@ export const init = async () => {
 	if (!el) return;
 
 	try {
-		const response = await fetch('https://api.meetup.com/frontendisti/events', {
-			mode: 'cors',
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-			},
-		});
+		const response = await fetch('https://api.meetup.com/frontendisti/events');
 
 		if (!response.ok) {
 			console.error(response);
